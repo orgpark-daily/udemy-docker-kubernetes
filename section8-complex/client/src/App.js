42a7b329df5dcb000e5c234f7560c035503a84fd
hp256@cornell.edu <hp256@cornell.edu>
2019-10-12 12:03:41 +0900

    section 8 done

---
+++ section8-complex/client/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import logo from './logo.svg';
+import './App.css';
+import OtherPage from './OtherPage';
+import Fib from './Fib';
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <Link to='/'>Home</Link>
+        <Link to='/otherpage'>Other Page</Link>
+      </header>
+      <div>
+        <Route exact path='/' component={Fib} />
+        <Route path='/otherpage' component={OtherPage} />
+      </div>
+    </div>
+  );
+}
+
+export default App;
